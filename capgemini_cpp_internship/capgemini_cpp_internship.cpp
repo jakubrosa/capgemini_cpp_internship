5861883d981e7b2e5acde36c1b0a6bf1b0bf4cd1
Jakub Rosa <contact@jakubrosa.dev>
2023-02-06 14:25:54 +0100

    Create a skeleton for sorting features.

--- capgemini_cpp_internship/capgemini_cpp_internship.cpp
+++ capgemini_cpp_internship/capgemini_cpp_internship.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 struct Cellphone
 {
@@ -15,6 +16,7 @@ struct Cellphone
 class CellphoneShop
 {
 public:
+
 	void addPhone()
 	{
 		Cellphone phoneToAdd;
@@ -35,10 +37,10 @@ public:
 		cellphones.push_back(phoneToAdd);
 	}
 
-	void deletePhone()
+	void showPhones()
 	{
 		int IndexNo = 0;
-		for (Cellphone i : cellphones) {
+		for (const auto& i : cellphones) {
 			std::cout << "Phone No. " << IndexNo << std::endl;
 			std::cout << "Brand name: " << i.brandName << std::endl;
 			std::cout << "Model name: " << i.modelName << std::endl;
@@ -48,11 +50,43 @@ public:
 			std::cout << "Price: " << i.price << std::endl;
 			IndexNo++;
 		}
+	}
+
+	void deletePhone()
+	{
+		showPhones();
 
 		std::cout << "Select a cellphone index number to delete: ";
 		int indexToDelete;
 		std::cin >> indexToDelete;
 		cellphones.erase(cellphones.begin() + indexToDelete);
+	}
+
+	void reports()
+	{
+		std::cout << "What kind of report do you want?" << std::endl;
+		std::cout << "1: Show all phones by the same brand." << std::endl;
+		std::cout << "2: Show all phones of a given form factor and screen size." << std::endl;
+		std::cout << "3: Show the name of the brand with the maximum quantity of the phones." << std::endl;
+		std::cout << "4: Show TOP - 3 screen sizes(popularity is determined by the number of phones with screen size)" << std::endl;
+		char option;
+		std::cin >> option;
+		switch (option)
+		{
+			case 1:
+				for (const auto &i : cellphones)
+				{
+					// Sadly I didn't have enough time for completion of this task, what I wanted to do was to use std::sort here.
+					// Also I know that this class takes too much responsibilities, but refactoring it would take some time ;-;
+				}
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4:
+				break;
+		}
 	}
 
 private:
@@ -67,10 +101,11 @@ int main()
 	bool flag = false;
 	while (true)
 	{
+		std::cout << "What operation do you want to perform?" << std::endl;
 		std::cout << "a: Add a phone." << std::endl;
 		std::cout << "d: Delete a phone." << std::endl;
+		std::cout << "r: Reports." << std::endl;
 		std::cout << "x: exit." << std::endl;
-		std::cout << "Please specify your operation: " << std::endl;
 		char option;
 		std::cin >> option;
 		switch (option)
@@ -80,6 +115,9 @@ int main()
 			break;
 		case 'd':
 			dummyShop.deletePhone();
+			break;
+		case 'r':
+			dummyShop.reports();
 			break;
 		case 'x':
 			flag = true;
