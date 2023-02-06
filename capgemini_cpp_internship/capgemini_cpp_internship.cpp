b9f8393361d68be5bedcc1853eb92eea4d371e93
Jakub Rosa <contact@jakubrosa.dev>
2023-02-06 13:57:11 +0100

    Create an application that imitate a cellphone shop with functionality to delete and remove phones from the shop.

--- capgemini_cpp_internship/capgemini_cpp_internship.cpp
+++ capgemini_cpp_internship/capgemini_cpp_internship.cpp
@@ -1,20 +1,92 @@
-// capgemini_cpp_internship.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+#include <vector>
+
+struct Cellphone
+{
+	std::string brandName;
+	std::string modelName;
+	std::string formFactor;
+	int yearOfIssue;
+	int screenSize;
+	int price;
+};
+
+class CellphoneShop
+{
+public:
+	void addPhone()
+	{
+		Cellphone phoneToAdd;
+
+		std::cout << "Enter brand name: " << std::endl;
+		std::cin >> phoneToAdd.brandName;
+		std::cout << "Enter model name: " << std::endl;
+		std::cin >> phoneToAdd.modelName;
+		std::cout << "Enter form factor: " << std::endl;
+		std::cin >> phoneToAdd.formFactor;
+		std::cout << "Enter year of issue: " << std::endl;
+		std::cin >> phoneToAdd.yearOfIssue;
+		std::cout << "Enter screen size: " << std::endl;
+		std::cin >> phoneToAdd.screenSize;
+		std::cout << "Enter price: " << std::endl;
+		std::cin >> phoneToAdd.price;
+		
+		cellphones.push_back(phoneToAdd);
+	}
+
+	void deletePhone()
+	{
+		int IndexNo = 0;
+		for (Cellphone i : cellphones) {
+			std::cout << "Phone No. " << IndexNo << std::endl;
+			std::cout << "Brand name: " << i.brandName << std::endl;
+			std::cout << "Model name: " << i.modelName << std::endl;
+			std::cout << "Form factor: " << i.formFactor << std::endl;
+			std::cout << "Year of issue: " << i.yearOfIssue << std::endl;
+			std::cout << "Screen size: " << i.screenSize << std::endl;
+			std::cout << "Price: " << i.price << std::endl;
+			IndexNo++;
+		}
+
+		std::cout << "Select a cellphone index number to delete: ";
+		int indexToDelete;
+		std::cin >> indexToDelete;
+		cellphones.erase(cellphones.begin() + indexToDelete);
+	}
+
+private:
+	std::vector<Cellphone> cellphones;
+};
+
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	CellphoneShop dummyShop;
+	bool flag = false;
+	while (true)
+	{
+		std::cout << "a: Add a phone." << std::endl;
+		std::cout << "d: Delete a phone." << std::endl;
+		std::cout << "x: exit." << std::endl;
+		std::cout << "Please specify your operation: " << std::endl;
+		char option;
+		std::cin >> option;
+		switch (option)
+		{
+		case 'a':
+			dummyShop.addPhone();
+			break;
+		case 'd':
+			dummyShop.deletePhone();
+			break;
+		case 'x':
+			flag = true;
+			break;
+		}
+		if (flag) break;
+	}
+
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
